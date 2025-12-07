60c4f21120811ac02ee06a3a2c765b7278bd1013
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-12-07 14:15:26 +0530

    Part 1 - Register and Login endpoints in the Presentation Layer

---
+++ BuberDinner/BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,26 @@
+﻿using BuberDinner.Contracts.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost]
+        [Route("register")]
+        public IActionResult Register(RegisterRequest request)
+        {
+            return Ok(request);
+        }
+
+
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(LoginRequest request)
+        {
+            return Ok(request);
+        }
+    }
+}
