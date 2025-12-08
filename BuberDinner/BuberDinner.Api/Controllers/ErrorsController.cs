450f8035d39430b3625ef0da31748ff5e3a909ad
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-12-08 20:19:11 +0530

    Part 5 ( Flow Control ) - via Exceptions

--- BuberDinner/BuberDinner.Api/Controllers/ErrorsController.cs
+++ BuberDinner/BuberDinner.Api/Controllers/ErrorsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Diagnostics;
+﻿using BuberDinner.Application.Common.Errors;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,7 +14,14 @@ namespace BuberDinner.Api.Controllers
         public IActionResult Error()
         {
             var exception = HttpContext.Features.Get<IExceptionHandlerFeature>().Error;
-            return Problem(statusCode:500, title:exception.Message);
+
+            var (statusCode, message) = exception switch
+            {
+                IServiceException serviceException => ((int)serviceException.StatusCode, serviceException.ErrorMessage),
+                _ => (StatusCodes.Status500InternalServerError, "An unexpected error occured")
+            };
+
+            return Problem(statusCode: statusCode, title: message);
         }
     }
 }
--- BuberDinner/BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner/BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,4 +1,5 @@
-﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+﻿using BuberDinner.Application.Common.Errors;
+using BuberDinner.Application.Common.Interfaces.Authentication;
 using BuberDinner.Application.Common.Interfaces.Persistence;
 using BuberDinner.Domain.Entities;
 
@@ -21,7 +22,7 @@ namespace BuberDinner.Application.Services.Authentication
             // Check if user already exists
             if(_userRepository.GetUserByEmail(email) is not null)
             {
-                throw new Exception("User with given email already exists.");
+                throw new DuplicateEmailException();
             }
 
             // Create user (generate uniqueId) & persist to DB
