eadb09294058852a83e4966d5302a10eba28b9f1
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-12-09 22:39:11 +0530

    add useExceptionHandler middleware

---
+++ BuberDinner/BuberDinner.Api/Controllers/ErrorsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers
+{
+
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ErrorsController : ControllerBase
+    {
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            var exception = HttpContext.Features.Get<IExceptionHandlerFeature>().Error;
+            return Problem(statusCode: 500, title: exception.Message);
+        }
+    }
+}
--- BuberDinner/BuberDinner.Api/Program.cs
+++ BuberDinner/BuberDinner.Api/Program.cs
@@ -23,6 +23,7 @@ var app = builder.Build();
         app.UseSwaggerUI(); 
     }
   
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
 
